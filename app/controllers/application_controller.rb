55626d2bc38637fe4ed93d77840f8d461c4bc8de
BlazingRockStorm <rocker.gryphon@gmail.com>
2019-03-11 16:29:05 +0700

    add redirect normal users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
     include Pundit
-    protect_from_forgery
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    protect_from_forgery with: :exception
+    
+    private
+        def user_not_authorized
+            redirect_to (root_path)
+        end
 end
