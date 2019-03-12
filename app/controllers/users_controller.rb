658d6fc8eb581121af15d432afbc69f2e9518316
BlazingRockStorm <rocker.gryphon@gmail.com>
2019-03-12 16:36:24 +0700

    change authorization and new form for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    authorize User
+    authorize @user
   end
 
   # GET /users/new
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    authorize User
+    authorize @user
   end
 
   # POST /users
