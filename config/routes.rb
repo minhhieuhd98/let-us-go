e8d5720b9cd52e54e24306e5ad499a4b47f249ae
BlazingRockStorm <rocker.gryphon@gmail.com>
2019-03-14 22:00:03 +0700

    add temp my calendar

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :comments
   devise_for :users
   resources :users
+  get 'users/:id/calendar' => 'users#calendar', as: 'calendar'
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
