7ec9017badb8e1a05898bbad671fc47dac0ad7c4
BlazingRockStorm <rocker.gryphon@gmail.com>
2019-03-25 08:16:52 +0700

    add delete comment

---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,16 @@
+class CommentPolicy < ApplicationPolicy
+    attr_reader :current_user, :comment
+  
+    def initialize(current_user, comment)
+      @current_user = current_user
+      @comment = comment
+    end
+
+    def update?
+      @current_user == @comment.user
+    end
+
+    def destroy?
+      current_user == comment.user
+    end
+end
