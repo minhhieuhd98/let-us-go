ef7c834917ead40d1733abd807910c808e10969a
BlazingRockStorm <rocker.gryphon@gmail.com>
2019-04-11 08:41:05 +0700

    add tags

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,20 @@ class Event < ApplicationRecord
     has_many :attendances
     has_many :comments
     has_many :users, through: :attendances
+    has_many :taggings
+    has_many :tags, through: :taggings
     has_many_attached :pictures
     scope :by_hidden_status, -> {where hidden_status: false }
     validates :start, presence: true, date: { after: Proc.new { Date.today + 3 }, message: "must be at least #{(Date.today + 4).strftime("%H:%M %Y/%m/%d").to_s}" }, on: :create
     validates :end, presence: true, date: { after:  :start}
+
+    def all_tags=(names)
+        self.tags = names.split(",").map do |name|
+            Tag.where(name: name.strip).first_or_create!
+        end
+    end
+    
+    def all_tags
+        self.tags.map(&:name).join(", ")
+    end
 end
