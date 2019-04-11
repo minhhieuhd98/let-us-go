293ca9bd0017fe43a5f41f16e0d262db1b66dcb8
BlazingRockStorm <rocker.gryphon@gmail.com>
2019-04-11 11:17:13 +0700

    add search by tag

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,4 +2,8 @@ module WelcomeHelper
     def time_format(time)
         time.strftime("%H:%M %Y/%m/%d")
     end
+    
+    def tag_links(tags)
+        tags.split(",").map{|tag| link_to tag.strip, tag_path(tag.strip) }.join(", ") 
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -18,4 +18,8 @@ class Event < ApplicationRecord
     def all_tags
         self.tags.map(&:name).join(", ")
     end
+
+    def self.tagged_with(name)
+        Tag.find_by_name!(name).events
+    end
 end
